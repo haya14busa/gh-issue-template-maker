c0fd8a107cacbc34c64791fab6420938f3116ba0
haya14busa <hayabusa1419@gmail.com>
2015-02-25 22:33:55 +0900

    gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -13,4 +14,11 @@ gulp.task('sass', function() {
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('js', function() {
+  gulp
+    .src(['assets/js/**/*.js', '!assets/js/min/**/*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js/min'));
 });
