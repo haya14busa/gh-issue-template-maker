615084f8335512e63b1078324427523de3b635e6
haya14busa <hayabusa1419@gmail.com>
2015-02-25 22:22:49 +0900

    gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -10,5 +11,6 @@ gulp.task('sass', function() {
   gulp
     .src('assets/sass/**/*scss')
     .pipe(sass())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('assets/css'));
 });
