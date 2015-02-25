782d44e8ed0567e144eb86186eb57b03a681e5bb
haya14busa <hayabusa1419@gmail.com>
2015-02-25 22:50:16 +0900

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,27 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
 
+// variables ---
+
+var source = {}; // files to watch
+source.js = ['assets/js/**/*.js', '!assets/js/min/**/*.js'];
+source.sass = 'assets/sass/**/*scss';
+
+// tasks ---
+
 gulp.task('default', function() {
   // place code for your default task here
   console.log('Hello gulp!');
 });
 
+gulp.task('watch', function() {
+  gulp.watch(source.js, ['js']);
+  gulp.watch(source.sass, ['sass']);
+});
+
 gulp.task('sass', function() {
   gulp
-    .src('assets/sass/**/*scss')
+    .src(source.sass)
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(gulp.dest('assets/css'));
@@ -18,7 +31,7 @@ gulp.task('sass', function() {
 
 gulp.task('js', function() {
   gulp
-    .src(['assets/js/**/*.js', '!assets/js/min/**/*.js'])
+    .src(source.js)
     .pipe(uglify())
     .pipe(gulp.dest('assets/js/min'));
 });
