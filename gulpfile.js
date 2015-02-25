4a1a71009593896d981185d0dc7e3a22294f44e2
haya14busa <hayabusa1419@gmail.com>
2015-02-25 22:18:32 +0900

    gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   // place code for your default task here
   console.log('Hello gulp!');
+});
+
+gulp.task('sass', function() {
+  gulp
+    .src('assets/sass/**/*scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'));
 });
